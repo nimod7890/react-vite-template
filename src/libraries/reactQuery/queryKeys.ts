ea96e701af4f350dc56a47995aba1b691824a055
nimod7890 <nimod1234@g.skku.edu>
2024-04-02 09:32:45 +0900

    delete: query keys

--- src/libraries/reactQuery/queryKeys.ts
+++ src/libraries/reactQuery/queryKeys.ts
@@ -1,21 +1,3 @@
-const enum QueryKeys {
-  User = "USER",
-  AdminUser = "ADMIN_USER",
-  Company = "COMPANY",
-  ClientCompanies = "CLIENT_COMPANIES",
-  StoreList = "STORE_LIST",
-  StoreLocationTypeList = "STORE_LOCATION_TYPE_LIST",
-  StoreUploadAvailability = "STORE_UPLOAD_AVAILABILITY",
-  ChannelList = "CHANNEL_LIST",
-  BrandList = "BRAND_LIST",
-  BrandCategoryList = "BRAND_CATEGORY_LIST",
-  ShopList = "SHOP_LIST",
-  UpdateShopConnection = "UPDATE_SHOP_CONNECTION",
-  ManagerList = "MANAGER_LIST",
-  Notice = "NOTICE",
-  NoticeList = "NOTICE_LIST",
-  NoticeUnread = "NOTICE_UNREAD",
-  InitChat = "INIT_CHAT",
-}
+const enum QueryKeys {}
 
 export default QueryKeys;
