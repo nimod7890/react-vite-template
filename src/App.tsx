51c698af4477ff8709287f78134c617d88be8885
nimod7890 <nimod1234@g.skku.edu>
2024-10-25 20:57:05 +0900

    feat: error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import AppRegister from "@libraries/index";
 import Router from "@routes/router";
+import GlobalErrorBoundary from "@components/layout/GlobalErrorBoundary";
+import GlobalFallback from "@components/layout/pending/GlobalFallback";
 
 export default function App() {
   return (
     <AppRegister>
-      <RouterProvider router={Router} fallbackElement={null} />
+      <GlobalErrorBoundary>
+        <RouterProvider router={Router} fallbackElement={<GlobalFallback />} />
+      </GlobalErrorBoundary>
     </AppRegister>
   );
 }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,51 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouteObject, createBrowserRouter } from "react-router-dom";
 import RoutePath from "@routes/routePath";
+import AuthProvider from "@/context/auth";
+import ErrorPage from "@pages/error/ErrorPage";
+import { Suspense } from "react";
+import GlobalFallback from "@components/layout/pending/GlobalFallback";
+import Layout from "@components/index";
+import NotFoundErrorPage from "@pages/error/NotFoundErrorPage";
+import root from "@routes/loader/root";
 
 const routes: RouteObject[] = [
   {
     path: RoutePath.Index,
-    element: <>index page</>,
+    loader: root,
+    errorElement: <ErrorPage message="알 수 없는 오류가 발생했어요!" />,
+    element: (
+      <Suspense fallback={<GlobalFallback />}>
+        <Outlet />
+      </Suspense>
+    ),
+    children: [
+      {
+        element: (
+          <AuthProvider>
+            <Layout />
+          </AuthProvider>
+        ),
+        children: [
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<GlobalFallback />}>
+                <div>hi</div>
+              </Suspense>
+            ),
+          },
+        ],
+      },
+    ],
   },
-  { path: "*", element: <>not found page</> },
+  {
+    path: RoutePath.Login,
+    element: <div>login</div>,
+  },
+  {
+    path: "*",
+    element: <NotFoundErrorPage />,
+  },
 ];
 
 const Router = createBrowserRouter(routes);
