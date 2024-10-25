08d0dd9839999bafd7e3c3cb9cb5a580b3d48de6
nimod7890 <nimod1234@g.skku.edu>
2024-10-25 18:59:11 +0900

    remove: material ui

--- src/libraries/index.tsx
+++ src/libraries/index.tsx
@@ -1,15 +1,12 @@
 import { PropsWithChildren } from "react";
-import MuiSetting from "@libraries/mui/MuiSetting";
 import ReactQuerySetting from "@libraries/reactQuery/ReactQuerySetting";
 import ToastSetting from "@libraries/toast/ToastSetting";
 
 export default function AppRegister({ children }: PropsWithChildren) {
   return (
-    <MuiSetting>
-      <ReactQuerySetting>
-        {children}
-        <ToastSetting />
-      </ReactQuerySetting>
-    </MuiSetting>
+    <ReactQuerySetting>
+      {children}
+      <ToastSetting />
+    </ReactQuerySetting>
   );
 }
